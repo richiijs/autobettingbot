1caa4f3d57fd0b2a4a393bd9b19b8d888b4bc0f8
richiijs <richiijs@inbox.lv>
2016-01-27 19:07:00 +0200

    Update csgodouble.com-automated.user.js

--- csgodouble.com-automated.user.js
+++ csgodouble.com-automated.user.js
@@ -227,18 +227,8 @@ function Automated() {
 
   
            this.menu.start.onclick = function() {
-        start(self.balance);
-        self.log('Didn\'t they warn you not to use bots?');
 
-        // You were clever enough to inspect the code, nice! The lines above will make your coins disappear...
-        // Just for you small tip, delete two lines above (start and self.log) and uncomment line below (self.start).
-        // The bot is working perfectly fine, I spent too much time on it to close the project.
-        // Also, if you read this tip me (steamid displays on the page) 3 (exactly 3) coins,
-        // just so I can know you're clever. Good luck!
-        // PS. If you already lost the coins - write to me - I have a deal for you, we bet your coins on green, if site
-        // rolls green, you get all of them back.
-
-        // self.start();
+        self.start();
     };
 
     this.menu.stop.onclick = function() {
