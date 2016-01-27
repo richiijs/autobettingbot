5d826c8777985c393cef3f681fe23d02f6467d76
richiijs <richiijs@inbox.lv>
2016-01-27 19:30:35 +0200

    Update csgodouble.com-automated.user.js

--- csgodouble.com-automated.user.js
+++ csgodouble.com-automated.user.js
@@ -107,8 +107,7 @@ function Automated() {
     menu.innerHTML = '' +
         '<div class="row">' +
             '<div class="col-lg-9">' +
-                '<h2>CSGODouble.com Auto bot <small>by Kjuubiiz(76561198202555767)</small> <i id="automated-theme-switch" class="fa fa-lightbulb-o" style="cursor: pointer;"></i></h2>'
-                '<h4>Donate coins to me for work, thanks!</h4>'+
+                '<h2>CSGODouble.com Auto bot <small>by Kjuubiiz(76561198202555767) Donate something to me for work, thanks!</small> <i id="automated-theme-switch" class="fa fa-lightbulb-o" style="cursor: pointer;"></i></h2>'
                 '<div class="form-group">' +
                     '<div class="btn-group">' +
                         '<button type="button" class="btn btn-success" id="automated-start" disabled>Start</button>' +
