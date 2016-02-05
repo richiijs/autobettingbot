a5d1d811d907cb3804ebf1f143e4a5a8ad116bf0
richiijs <richiijs@inbox.lv>
2016-02-05 23:49:09 +0200

    Update
    Update

--- csgodouble.com-automated.user.js
+++ csgodouble.com-automated.user.js
@@ -2,7 +2,7 @@
 // @name            csgodouble.com - auto betting bot
 // @description     An userscript that automates csgodouble.com betting using much systems.
 // @namespace       auto bot@kjuubiiz
-// @version         1.40
+// @version         1.41
 // @author          Kjuubiiz
 // @match           http://www.csgodouble.com/
 // @match           http://www.csgodouble.com/index.php
